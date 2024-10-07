9a049d0a5cb2837dc0fab625a5042b3c82ee17e4
Isuru Fernando <isuruf@gmail.com>
2024-10-07 17:28:48 -0500

    Split to lib and python packages

---
+++ recipe/build.sh
@@ -0,0 +1,14 @@
+# For CPU builds, we include OpenCL too. USE_GPU means OpenCL and not CUDA builds
+if [[ "$cuda_compiler_version" == "None" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DUSE_GPU=1"
+else
+  export CMAKE_ARGS="${CMAKE_ARGS} -DUSE_CUDA=1"
+fi
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -DBUILD_CLI=OFF ..
+make -j${CPU_COUNT}
+make install
+
