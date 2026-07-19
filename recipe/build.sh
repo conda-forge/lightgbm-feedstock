d1fe77da0b73b750203c09b3d8d93a2a545a64de
James Lamb <jaylamb20@gmail.com>
2026-07-19 15:25:00 -0500

    use NCCL shared library (upstream defaults to static linking)

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ fi
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -DBUILD_CLI=OFF -DUSE_HOMEBREW_FALLBACK=OFF ..
+cmake ${CMAKE_ARGS} -DBUILD_CLI=OFF -DBUILD_WITH_SHARED_NCCL=ON -DUSE_HOMEBREW_FALLBACK=OFF ..
 make -j${CPU_COUNT}
 make install
 
